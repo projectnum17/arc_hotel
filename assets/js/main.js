46cbe5338f90766646ba6109b37257fbf53c8cdf
projectnum17 <egor.chornyy@gmail.com>
2025-11-10 14:39:05 +0200

    add logo animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,12 @@ import helpers from './mods/helpers.js';
 import header from './mods/header.js';
 import videoAsync from './mods/videoAsync.js';
 import slidersConfig from './mods/slidersConfig.js';
+import mainLogoAnimation from './mods/mainLogoAnimation.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     helpers();
     header();
     videoAsync();
     slidersConfig();
+    mainLogoAnimation();
 });
