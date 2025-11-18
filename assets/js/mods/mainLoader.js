fa6e10051c984a9e76dcc5ef02d3dab560f9b0cb
projectnum17 <egor.chornyy@gmail.com>
2025-11-18 18:55:10 +0200

    fix loader script

--- assets/js/mods/mainLoader.js
+++ assets/js/mods/mainLoader.js
@@ -21,11 +21,18 @@ const mainLoader = () => {
         animatedItems.forEach((item) => observer.observe(item));
     };
 
+    const loaderExists =
+        document.querySelector('.js-loader') ||
+        document.querySelector('.js-loader-text') ||
+        document.querySelector('.js-loader-logo');
+
     const pic = document.querySelector('.js-hero-pic');
     const box = document.querySelector('.js-hero-box');
     const panel = document.querySelector('.js-hero-panel');
 
-    document.body.classList.add('is-locked');
+    if (loaderExists) {
+        document.body.classList.add('is-locked');
+    }
 
     animItems('.js-loader-text');
     animItems('.js-loader-logo');
@@ -37,7 +44,10 @@ const mainLoader = () => {
             pic?.classList.add('is-active');
             box?.classList.add('is-active');
             panel?.classList.add('is-active');
-            document.body.classList.remove('is-locked');
+
+            if (loaderExists) {
+                document.body.classList.remove('is-locked');
+            }
         }, 500);
     }, 1000);
 };
