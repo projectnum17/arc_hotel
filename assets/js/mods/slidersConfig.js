d90dec16a99950429e1aa4b0b21e048fc461fd57
projectnum17 <egor.chornyy@gmail.com>
2025-11-19 09:47:54 +0200

    adaptive room page

--- assets/js/mods/slidersConfig.js
+++ assets/js/mods/slidersConfig.js
@@ -116,6 +116,20 @@ const slidersConfig = () => {
                 el: '.js-concept-progress',
                 type: 'progressbar',
             },
+            breakpoints: {
+                0: {
+                    spaceBetween: 12,
+                    slidesPerView: 1,
+                },
+                768: {
+                    spaceBetween: 30,
+                    slidesPerView: 2,
+                },
+                992: {
+                    spaceBetween: 40,
+                    slidesPerView: 2.5,
+                },
+            },
         });
     };
 
